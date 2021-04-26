57d87c4a32a7b3f5fbb69bbb78caf2544d1cd7ec
hitblunders <mouli.mondal145@gmail.com>
2021-04-26 15:02:27 +0530

    bot

--- bot.py
+++ bot.py
@@ -44,4 +44,4 @@ def get_response(msg):
             if tag == intent["tag"]:
                 return random.choice(intent['responses'])
 
-    return "I do not understand..."
+    return "Sorry. I do not understand..."
